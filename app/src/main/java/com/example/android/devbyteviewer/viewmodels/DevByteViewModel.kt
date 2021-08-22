591c10c1486d60ec326c176e344cbb1ee7f7767a
Amartya Mondal <atm1504.in@gmail.com>
2021-08-22 22:59:09 +0530

    Impleent work manager

--- app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
+++ app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
@@ -1,38 +1,27 @@
 package com.example.android.devbyteviewer.viewmodels
 
 import android.app.Application
-import androidx.lifecycle.*
-import com.example.android.devbyteviewer.domain.Video
-import com.example.android.devbyteviewer.network.Network
-import com.example.android.devbyteviewer.network.asDomainModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
 import kotlinx.coroutines.launch
-import java.io.IOException
 
 class DevByteViewModel(application: Application) : AndroidViewModel(application) {
 
-
-    private val _playlist = MutableLiveData<List<Video>>()
-
-    val playlist: LiveData<List<Video>>
-        get() = _playlist
+    private val database = getDatabase(application)
+    private val videosRepository = VideosRepository(database)
 
     init {
-        refreshDataFromNetwork()
-    }
-
-
-    private fun refreshDataFromNetwork() = viewModelScope.launch {
-        try {
-            val playlist = Network.devbytes.getPlaylist().await()
-            _playlist.postValue(playlist.asDomainModel())
-        } catch (networkError: IOException) {
-            // Show an infinite loading spinner if the request fails
-            // challenge exercise: show an error to the user if the network request fails
+        viewModelScope.launch {
+            videosRepository.refreshVideos()
         }
     }
+
+    val playlist = videosRepository.videos
+
 
     class Factory(val app: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
