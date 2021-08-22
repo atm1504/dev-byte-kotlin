098913c506cb51c01ee62795478b534ed2652ac8
Amartya Mondal <atm1504.in@gmail.com>
2021-08-22 20:54:45 +0530

    Add data

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -7,7 +7,7 @@ import androidx.room.*
 @Dao
 interface VideoDao {
     @Query("select * from databasevideo")
-    fun getVideos(): LiveData<DatabaseVideo>
+    fun getVideos(): LiveData<List<DatabaseVideo>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DatabaseVideo)
